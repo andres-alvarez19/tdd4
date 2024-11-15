f802be1b7680683427d0efa8587e8a85ce508fcd
andres-alvarez19 <andresalvarez360@gmail.com>
2024-11-15 12:01:23 -0300

    Agregando funcionalidad para aprobar hasta el test 3 de kaprekarOp. Se modifica la documentacion para explicar el retorno de la funcion

--- src/main/java/com/example/dev/Main_Kaprekar.java
+++ src/main/java/com/example/dev/Main_Kaprekar.java
@@ -4,12 +4,14 @@ package com.example.dev;
 public class Main_Kaprekar {
     /*
         * @param number número a operar con el algoritmo de Kaprekar
-        * @return retorna una constante 3087 si el numero es 3524 y en cualquier otro caso devuelve 999
+        * @return retorna una constante 3087 si el numero es 3524, 5175 si number es 5200 y en cualquier otro caso devuelve 999
      */
     public static int kaprekarOp(int number) {
         if (number == 3524) {
             return 3087;
-        }else {
+        }else if (number == 5200) {
+            return 5175;
+        }else{
             return 999;
         }
     }
